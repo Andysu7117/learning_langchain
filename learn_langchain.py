ad3ba4e6130d5f358fd830a21d7f3aa1fcddfdab
Andy <andy.su7117@gmail.com>
2026-06-14 21:11:51 +1000

    adding in a few-shot prompt

--- learn_langchain.py
+++ learn_langchain.py
@@ -77,3 +77,28 @@ print(f"response : {response}\n")
 response = llm_model(zero_shot_prompt, params)
 print(f"prompt: {zero_shot_prompt}\n")
 print(f"response : {response}\n")
+
+params = {
+    "max_new_tokens": 10,
+}
+
+few_shot_prompt = """Here are few examples of classifying emotions in statements:
+
+            Statement: 'I just won my first marathon!'
+            Emotion: Joy
+            
+            Statement: 'I can't believe I lost my keys again.'
+            Emotion: Frustration
+            
+            Statement: 'My best friend is moving to another country.'
+            Emotion: Sadness
+            
+            Now, classify the emotion in the following statement:
+            Statement: 'That movie was so scary I had to cover my eyes.’
+            
+
+"""
+
+response = llm_model(few_shot_prompt, params)
+print(f"prompt: {few_shot_prompt}\n")
+print(f"response : {response}\n")
