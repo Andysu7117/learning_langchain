de5e0b462c9d7da55c53e838b4aec2602f90b42e
Andy <andy.su7117@gmail.com>
2026-06-15 11:54:42 +1000

    initial rag implementation

---
+++ rag.py
@@ -0,0 +1,52 @@
+from langchain_core.documents import Document
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain_ibm import WatsonxEmbeddings
+from ibm_watsonx_ai.metanames import EmbedTextParamsMetaNames
+from langchain.chains import RetrievalQA
+
+# 1. Load a document about AI
+loader = WebBaseLoader("https://python.langchain.com/v0.2/docs/introduction/")
+documents = ##TODO
+
+# 2. Split the document into chunks
+text_splitter = ##TODO
+chunks = ##TODO
+
+# 3. Set up the embedding model. (Use an embedding model to create vector representations.)
+embed_params = {
+    EmbedTextParamsMetaNames.TRUNCATE_INPUT_TOKENS: 3,
+    EmbedTextParamsMetaNames.RETURN_OPTIONS: {"input_text": True},
+}
+
+embedding_model = ##TODO: use ibm/slate-125m-english-rtrvr-v2 model
+)
+
+# 4. Create a vector store
+vector_store = ##TODO
+
+# 5. Create a retriever
+retriever = ##TODO
+
+# 6. Define a function to search for relevant information
+def search_documents(query, top_k=3):
+    """Search for documents relevant to a query"""
+    # Use the retriever to get relevant documents
+    docs = retriever.get_relevant_documents(query)
+    
+    # Limit to top_k if specified
+    return docs[:top_k]
+
+# 7. Test with a few queries
+test_queries = [
+    "What is LangChain?",
+    "How do retrievers work?",
+    "Why is document splitting important?"
+]
+
+for query in test_queries:
+    print(f"\nQuery: {query}")
+    results = ##TODO
+    # Print the results
+    ##TODO: Display the results clearly
